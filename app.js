7d821b8535e7af03bb15cfc74f62c22488afd3dd
Timothy Lenardo <tim@Timothys-MacBook-Pro-2.local>
2018-08-24 07:29:03 -0700

    try an npm install prebuild

--- app.js
+++ app.js
@@ -31,8 +31,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static('gatsby/public'))
 
-// app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
+
+app.get('*', function(req, res) {
+    res.sendfile('./gatsby/public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
